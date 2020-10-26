2f96172143350423c004e32b22fd3754ab04b315
Joshua Whelan <josh25whelan@gmail.com>
2020-10-26 11:49:12 -0400

    now it can show a random dog

--- index.js
+++ index.js
@@ -3,8 +3,19 @@
 function getDogImage() {
   fetch('https://dog.ceo/api/breeds/image/random')
     .then(response => response.json())
-    .then(responseJson => console.log(responseJson));
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => alert('Something went wrong. Try again later.'))
 }
+
+function displayResults(responseJson) {
+    console.log(responseJson);
+    //replace the existing image with the new one
+    $('.results-img').replaceWith(
+      `<img src="${responseJson.message}" class="results-img">`
+    )
+    //display the results section
+    $('.results').removeClass('hidden');
+  }
 
 function watchForm() {
   $('form').submit(event => {
@@ -13,7 +24,9 @@ function watchForm() {
   });
 }
 
-$(function() {
+function start () {
   console.log('App loaded! Waiting for submit!');
   watchForm();
-});
+};
+
+$(start())
